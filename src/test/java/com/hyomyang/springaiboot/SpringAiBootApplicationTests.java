767b0ced35adf0900d80eff0b5d15295aa2d200e
HyomYom <hyomyang@gmail.com>
2025-11-08 15:20:23 +0900

    fix: test 강제실행 방지 코드 수정

--- src/test/java/com/hyomyang/springaiboot/SpringAiBootApplicationTests.java
+++ src/test/java/com/hyomyang/springaiboot/SpringAiBootApplicationTests.java
@@ -3,7 +3,7 @@ package com.hyomyang.springaiboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringAiBootApplicationTests {
 
     @Test
