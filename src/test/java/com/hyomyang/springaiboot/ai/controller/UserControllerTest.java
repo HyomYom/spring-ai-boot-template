113835e04a8a2a52b5ec015754b08f41bfa404ee
HyomYom <hyomyang@gmail.com>
2025-12-28 22:03:23 +0900

    test: test환경 db 재설정

--- src/test/java/com/hyomyang/springaiboot/ai/controller/UserControllerTest.java
+++ src/test/java/com/hyomyang/springaiboot/ai/controller/UserControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 import org.mockito.Mockito;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @ExtendWith(TestLogger.class)
+@ActiveProfiles("test")
 @Slf4j
 public class UserControllerTest implements TestWatcher {
 
